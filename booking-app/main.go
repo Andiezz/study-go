bd4e31a7de04c04db5a1e91d6ff82e1442439da8
Vũ Thành An <superlegend27@gmail.com>
2024-07-14 10:01:58 +0700

    Finished first lesson by Nana

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
+	"time"
+	"sync"
+	// "strconv"
 	// "booking-app/helper"
 )
 
@@ -15,41 +17,101 @@ var remainingTickets uint = 50
 
 // slice
 // var bookings = []string{} // or: bookings := []string{}
-var bookings = make([]map[string]string, 0)
+// var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
 
+type UserData struct {
+	userName        string
+	email           string
+	numberOfTickets uint
+}
+
+// func main() {
+// 	greetUsers()
+
+// 	// infinite loop -> for {}
+// 	// conditional loop -> for condition {}
+// 	for remainingTickets > 0 && len(bookings) < 50 {
+// 		userName, email, userTickets := getUserInput()
+// 		// isValidName, isValidEmail, isValidTicket := helper.ValidateUserInput(userName, email, userTickets, remainingTickets)
+// 		isValidName, isValidEmail, isValidTicket := validateUserInput(userName, email, userTickets)
+
+// 		if !isValidName || !isValidEmail || !isValidTicket {
+// 			if !isValidName {
+// 				fmt.Println("Invalid name")
+// 			}
+// 			if !isValidEmail {
+// 				fmt.Println("Invalid email")
+// 			}
+// 			if !isValidTicket {
+// 				fmt.Println("Invalid ticket number")
+// 			}
+// 			continue
+// 		}
+
+// 		bookTicket(userName, email, userTickets)
+// 		go sendTicket(userTickets, userName, email)
+
+// 		// call function print first name
+// 		firstNames := getFirstName()
+// 		fmt.Printf("The first names of booking are: %v\n", firstNames)
+
+// 		if remainingTickets == 0 {
+// 			fmt.Println("All tickets are sold out!")
+// 			break
+// 		}
+// 	}
+
+// 	city := "London"
+
+// 	switch city {
+// 	case "London", "Berlin":
+// 		fmt.Println("The conference is in London")
+// 	case "Singapore":
+// 		fmt.Println("The conference is in Singapore")
+// 	case "Paris":
+// 		fmt.Println("The conference is in Paris")
+// 	default:
+// 		fmt.Println("Invalid city is entered!")
+// 	}
+// }
+
+var wg = sync.WaitGroup {}
+
+// synchronize Goroutines
 func main() {
 	greetUsers()
 
 	// infinite loop -> for {}
 	// conditional loop -> for condition {}
-	for remainingTickets > 0 && len(bookings) < 50 {
-		userName, email, userTickets := getUserInput()
-		// isValidName, isValidEmail, isValidTicket := helper.ValidateUserInput(userName, email, userTickets, remainingTickets)
-		isValidName, isValidEmail, isValidTicket := validateUserInput(userName, email, userTickets)
 
-		if !isValidName || !isValidEmail || !isValidTicket {
-			if !isValidName {
-				fmt.Println("Invalid name")
-			}
-			if !isValidEmail {
-				fmt.Println("Invalid email")
-			}
-			if !isValidTicket {
-				fmt.Println("Invalid ticket number")
-			}
-			continue
+	userName, email, userTickets := getUserInput()
+	// isValidName, isValidEmail, isValidTicket := helper.ValidateUserInput(userName, email, userTickets, remainingTickets)
+	isValidName, isValidEmail, isValidTicket := validateUserInput(userName, email, userTickets)
+
+	if !isValidName || !isValidEmail || !isValidTicket {
+		if !isValidName {
+			fmt.Println("Invalid name")
 		}
-
-		bookTicket(userName, email, userTickets)
-
-		// call function print first name
-		firstNames := getFirstName()
-		fmt.Printf("The first names of booking are: %v\n", firstNames)
-
-		if remainingTickets == 0 {
-			fmt.Println("All tickets are sold out!")
-			break
+		if !isValidEmail {
+			fmt.Println("Invalid email")
 		}
+		if !isValidTicket {
+			fmt.Println("Invalid ticket number")
+		}
+	}
+
+	bookTicket(userName, email, userTickets)
+
+	wg.Add(1)
+	go sendTicket(userTickets, userName, email)
+
+	// call function print first name
+	firstNames := getFirstName()
+	fmt.Printf("The first names of booking are: %v\n", firstNames)
+
+	if remainingTickets == 0 {
+		fmt.Println("All tickets are sold out!")
 	}
 
 	city := "London"
@@ -64,6 +126,7 @@ func main() {
 	default:
 		fmt.Println("Invalid city is entered!")
 	}
+	wg.Wait()
 }
 
 func greetUsers() {
@@ -79,7 +142,7 @@ func getFirstName() []string {
 
 	// for each loop -> index, element := range array {}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking["name"])
+		var names = strings.Fields(booking.userName)
 		firstNames = append(firstNames, names[0])
 	}
 	fmt.Printf("These are all the bookings: %v\n", bookings)
@@ -108,10 +171,13 @@ func bookTicket(userName string, email string, userTickets uint) {
 
 	// create a map for a user
 	// make empty map -> make(map[keyType]valueType)
-	var userdata = make(map[string]string)
-	userdata["name"] = userName + " Vu"
-	userdata["email"] = email
-	userdata["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	// var userdata = make(map[string]string)
+	var userdata = UserData{
+		userName:        userName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+	// userdata["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
 
 	bookings = append(bookings, userdata)
 
@@ -122,4 +188,13 @@ func bookTicket(userName string, email string, userTickets uint) {
 
 	fmt.Printf("User %v with the %v books %v tickets.\n", userName, email, userTickets)
 	fmt.Printf("Remaining tickets: %v\n", remainingTickets)
+}
+
+func sendTicket(userTickets uint, userName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickers for %v", userTickets, userName)
+	fmt.Println("################")
+	fmt.Printf("Sending ticket:\n %v \n to email addess %v\n", ticket, email)
+	fmt.Println("################")
+	wg.Done()
 }
